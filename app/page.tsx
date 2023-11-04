bb1910736eab6f08fb8e800740909d777164e639
Bijen Shrestha <shrbijen31@gmail.com>
2023-11-04 12:17:15 +0545

    travel app

---
+++ app/page.tsx
@@ -0,0 +1,13 @@
+import React from 'react'
+
+const Homepage = () => {
+  return (
+    <div className='font-bold text-teal-500 text-2xl'>
+      
+      Homepage
+
+    </div>
+  )
+}
+
+export default Homepage
